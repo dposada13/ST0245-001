69f847d54cff77180ca84c5e66f174bff8b634ac
dposada13 <72612937+dposada13@users.noreply.github.com>
2021-02-26 11:45:47 -0500

    Add files via upload

---
+++ talleres/taller05/Taller5.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Taller5 {  
+
+    /**
+     * @param array es una arreglo de numeros enteros.
+     * El método suma tiene la intención de hacer el proceso de suma
+     * mediante una funcion cíclica (while/for/...)
+     * @return la suma de todos los numeros sumados.
+     */
+    public static int suma (int[]array){
+        int sum=0;
+        for (int i=0; i<array.length; i++){
+            sum=sum+array[i];
+        }
+        return sum;
+    }
+
+    /**
+     * @param num es el numero el cual se utiliza para ser multiplicado.
+     * El método mul tiene la intención de hacer la multiplicación
+     * de 1 a n por el numero mul
+     * mediante una funcion cíclica (while/for/...)
+     * 
+     */
+    public static void mul (int num){
+        int sum=0;
+        for (int i=0; i<=10; i++){
+            sum=num*i;
+            System.out.println(i+"*"+num+"= "+sum); 
+        }
+
+    }
+
+    /**
+     * @param array es un arreglo de números desordenados
+     * El método insertionSort tiene la intención ordenar los números
+     * del arreglo array por el método insertion:
+     * @see <a href="https://www.youtube.com/watch?v=OGzPmgsI-pQ"> Insertion Sort <a/>
+     * mediante la anidación de funciones cíclicas (while/for/...)
+     * 
+     */
+    public static int[] insertionSort (int[] array){
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+            }  
+            array[i+1] = key;  
+        } 
+        return array;
+    }    
+}
